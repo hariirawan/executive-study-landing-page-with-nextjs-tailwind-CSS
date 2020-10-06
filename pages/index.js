3e53062ef5b76a4de3e2daa0f12c7368bc49e68d
Hari Irawan <hari31121993@gmail.com>
2020-10-06 14:26:14 +0700

    Update Index and apps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Events from "../components/Events";
+import Feature from "../components/Feature";
+import Hero from "../components/Hero";
 import Nav from "../components/Navbar";
+import Services from "../components/Services";
 
 export default function IndexPage() {
   return (
-    <div>
+    <div className="bg-main-color pb-12">
       <Nav />
+      <Hero />
+      <Feature />
+      <Services />
+      <Events />
     </div>
   );
 }

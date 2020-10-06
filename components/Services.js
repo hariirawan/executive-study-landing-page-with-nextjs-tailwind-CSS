d3679dc9781e28286fe6d44fba14a38820f6db75
Hari Irawan <hari31121993@gmail.com>
2020-10-06 14:25:55 +0700

    Create Services

---
+++ components/Services.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function Services(props) {
+  return (
+    <div className="bg-main-color">
+      <div className="container mx-auto lg:px-24 py-24 space-y-12">
+        <div className="flex justify-center">
+          <div className="w-2/3 space-y-6">
+            <div className="text-center text-2xl font-semibold">
+              Our Services
+            </div>
+            <div className="text-center ">
+              Executive Trade International is an University Application Centre,
+              guiding Bangladeshi students to Australian, British, Canadian and
+              Irish universities!
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <div className="bg-white p-5 rounded-lg w-1/4 mx-4 my-4  space-y-6">
+            <div className="h-12 w-12 rounded-full bg-indigo-500"></div>
+            <div>
+              <h1 className="text-lg font-semibold">Career Counselling</h1>
+              <p>
+                Maiores voluptas laboriosam non dolorum perferendis fuga
+                repellat aut. Blanditiis quos in minus.
+              </p>
+            </div>
+            <div className="text-indigo-600 font-semibold">Learn More</div>
+          </div>
+          <div className="bg-white p-5 w-1/4  rounded-lg  mx-4 my-4  space-y-6 ">
+            <div className="h-12 w-12 rounded-full bg-red-500"></div>
+            <div>
+              <h1 className="text-lg font-semibold">Career Counselling</h1>
+              <p>
+                Maiores voluptas laboriosam non dolorum perferendis fuga
+                repellat aut. Blanditiis quos in minus.
+              </p>
+            </div>
+            <div className="text-indigo-600 font-semibold">Learn More</div>
+          </div>
+          <div className="bg-white p-5 w-1/4  rounded-lg  mx-4 my-4  space-y-6 ">
+            <div className="h-12 w-12 rounded-full bg-green-500"></div>
+            <div>
+              <h1 className="text-lg font-semibold">Career Counselling</h1>
+              <p>
+                Maiores voluptas laboriosam non dolorum perferendis fuga
+                repellat aut. Blanditiis quos in minus.
+              </p>
+            </div>
+            <div className="text-indigo-600 font-semibold">Learn More</div>
+          </div>
+          <div className="bg-white p-5 w-1/4  rounded-lg  mx-4 my-4  space-y-6 ">
+            <div className="h-12 w-12 rounded-full bg-blue-500"></div>
+            <div>
+              <h1 className="text-lg font-semibold">Career Counselling</h1>
+              <p>
+                Maiores voluptas laboriosam non dolorum perferendis fuga
+                repellat aut. Blanditiis quos in minus.
+              </p>
+            </div>
+            <div className="text-indigo-600 font-semibold">Learn More</div>
+          </div>
+          <div className="bg-white p-5 w-1/4  rounded-lg  mx-4 my-4  space-y-6 ">
+            <div className="h-12 w-12 rounded-full bg-orange-600"></div>
+            <div>
+              <h1 className="text-lg font-semibold">Career Counselling</h1>
+              <p>
+                Maiores voluptas laboriosam non dolorum perferendis fuga
+                repellat aut. Blanditiis quos in minus.
+              </p>
+            </div>
+            <div className="text-indigo-600 font-semibold">Learn More</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
